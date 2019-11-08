16b5298a2f4afc9a4894a1edd6fa8c9d10bfc184
Thomas Boerger <thomas@webhippie.de>
2019-11-08 12:36:45 +0100

    Add generated frontend client

---
+++ ui/client/hello/index.js
@@ -0,0 +1,75 @@
+/* eslint-disable */
+import axios from 'axios'
+import qs from 'qs'
+let domain = ''
+export const getDomain = () => {
+  return domain
+}
+export const setDomain = ($domain) => {
+  domain = $domain
+}
+export const request = (method, url, body, queryParameters, form, config) => {
+  method = method.toLowerCase()
+  let keys = Object.keys(queryParameters)
+  let queryUrl = url
+  if (keys.length > 0) {
+    queryUrl = url + '?' + qs.stringify(queryParameters)
+  }
+  // let queryUrl = url+(keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '')
+  if (body) {
+    return axios[method](queryUrl, body, config)
+  } else if (method === 'get') {
+    return axios[method](queryUrl, config)
+  } else {
+    return axios[method](queryUrl, qs.stringify(form), config)
+  }
+}
+/*==========================================================
+ *                    
+ ==========================================================*/
+/**
+ * 
+ * request: Greet
+ * url: GreetURL
+ * method: Greet_TYPE
+ * raw_url: Greet_RAW_URL
+ * @param body - 
+ */
+export const Greet = function(parameters = {}) {
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  const config = parameters.$config
+  let path = '/api/v0alpha1/greet'
+  let body
+  let queryParameters = {}
+  let form = {}
+  if (parameters['body'] !== undefined) {
+    body = parameters['body']
+  }
+  if (parameters['body'] === undefined) {
+    return Promise.reject(new Error('Missing required  parameter: body'))
+  }
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    });
+  }
+  return request('post', domain + path, body, queryParameters, form, config)
+}
+export const Greet_RAW_URL = function() {
+  return '/api/v0alpha1/greet'
+}
+export const Greet_TYPE = function() {
+  return 'post'
+}
+export const GreetURL = function(parameters = {}) {
+  let queryParameters = {}
+  const domain = parameters.$domain ? parameters.$domain : getDomain()
+  let path = '/api/v0alpha1/greet'
+  if (parameters.$queryParameters) {
+    Object.keys(parameters.$queryParameters).forEach(function(parameterName) {
+      queryParameters[parameterName] = parameters.$queryParameters[parameterName]
+    })
+  }
+  let keys = Object.keys(queryParameters)
+  return domain + path + (keys.length > 0 ? '?' + (keys.map(key => key + '=' + encodeURIComponent(queryParameters[key])).join('&')) : '')
+}
