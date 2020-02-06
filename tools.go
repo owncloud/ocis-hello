67b49fb3afafccf5193c91496cf3e61aca0f251f
David Christofas <dchristofas@owncloud.com>
2020-02-06 14:02:55 +0100

    track tool depencies
    Added tracking for tool dependencies to be able to run `go mod tidy` without losing them.
    More information: https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module

---
+++ tools.go
@@ -0,0 +1,12 @@
+// +build tools
+
+package tools
+
+import (
+	"github.com/UnnoTed/fileb0x"
+	"github.com/cespare/reflex"
+	"github.com/haya14busa/goverage"
+	"github.com/kballard/go-shellquote"
+	"github.com/ogier/pflag"
+	"github.com/restic/calens"
+)
