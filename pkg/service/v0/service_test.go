a71c2bd38bea80cd27942d32bb4db1b2259b895c
Artur Neumann <artur@jankaritech.com>
2020-04-28 17:21:12 +0545

    unit test for service

---
+++ pkg/service/v0/service_test.go
@@ -0,0 +1,38 @@
+package svc
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	v0proto "github.com/owncloud/ocis-hello/pkg/proto/v0"
+)
+
+func TestHello_Greet(t *testing.T) {
+	tests := []struct {
+		name                 string
+		req                  string
+		expectedErrorMessage interface{}
+	}{
+		{"simple", "simple", nil},
+		{"UTF", "मिलन", nil},
+		{"special char", `%&# /\`, nil},
+		{"empty", "", "missing a name"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Hello{}
+			req := &v0proto.GreetRequest{Name: tt.req}
+			var rsp = &v0proto.GreetResponse{}
+
+			err := s.Greet(context.Background(), req, rsp)
+
+			if tt.expectedErrorMessage != nil || err != nil {
+				assert.EqualError(t, err, tt.expectedErrorMessage.(string))
+			} else {
+				assert.Equal(t, "Hello "+tt.req, rsp.Message)
+			}
+		})
+	}
+}
